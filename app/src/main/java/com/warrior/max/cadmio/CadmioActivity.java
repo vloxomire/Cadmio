866ac0d3a05257b16e479721aa48d38b7c83d107
Max <vloxomire@gmail.com>
2018-08-08 21:17:42 -0300

    Cadmio inicial
    Inicio del repositorio

---
+++ app/src/main/java/com/warrior/max/cadmio/CadmioActivity.java
@@ -0,0 +1,13 @@
+package com.warrior.max.cadmio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadmioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadmio);
+    }
+}
