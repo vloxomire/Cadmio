fffd02ea383da35b983a6bff7cb03f1fff0cead5
MakusuMax <40814622+vloxomire@users.noreply.github.com>
2018-08-09 09:52:55 -0300

    constructor
    creo el constructor de Bguardar

--- app/src/main/java/com/warrior/max/cadmio/Listeners/ListenerBGuardar.java
+++ app/src/main/java/com/warrior/max/cadmio/Listeners/ListenerBGuardar.java
@@ -1,4 +1,14 @@
 package com.warrior.max.cadmio.Listeners;
 
-public class ListenerBGuardar {
+import android.view.View;
+import android.content.Intent;
+
+import com.warrior.max.cadmio.Interfaz.CadmioActivity;
+
+public class ListenerBGuardar implements View.OnClickListener {
+    CadmioActivity context;
+
+    public ListenerBGuardar(CadmioActivity context) { //creo el CONSTRUCTOR
+        this.context = context;
+    }
 }
